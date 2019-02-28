cceafedacf4f94f43946edcb75276892f19c252a
Mitchell de Rijcke <mdr@rijx.com>
2019-02-28 13:39:26 +0100

    Build: Simplify Git release

--- .releaserc.js
+++ .releaserc.js
@@ -2,11 +2,6 @@ module.exports = {
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "eslint" }],
     "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        message: "${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ]
+    "@semantic-release/git"
   ]
 };
