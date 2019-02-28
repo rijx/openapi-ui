7bc911868c5163c994f3ba7033a6add21ee60f04
Mitchell de Rijcke <mdr@rijx.com>
2019-02-28 13:20:26 +0100

    Build: Add proper tagging

--- .releaserc.js
+++ .releaserc.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "eslint" }],
-    "@semantic-release/npm"
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        message: "${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
   ]
 };
